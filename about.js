ff9b66f211afe468d3b127b3e0274544912aa60b
omar-programme <139594772+omar-programme@users.noreply.github.com>
2023-10-06 22:39:31 +0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,28 @@
+document.querySelector('.button').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.add('active');
+  });
+  
+  document.querySelector('.close-btn').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.remove('active');
+  });
+
+  //2
+  
+  document.querySelector('.buttonn').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.add('active');
+  });
+  
+  document.querySelector('.close-btn').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.remove('active');
+  });
+
+  //3
+  
+  document.querySelector('.buttonnn').addEventListener('click', function() {
+    document.querySelector('.overlayyy').classList.add('active');
+  });
+
+  document.querySelector('.close-btnn').addEventListener('click', function() {
+    document.querySelector('.overlayyy').classList.remove('active');
+  });
+
---
+++ contact.js
@@ -0,0 +1,27 @@
+document.querySelector('.button').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.add('active');
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.remove('active');
+});
+
+// 2
+document.querySelector('.bttn').addEventListener('click', function() {
+    document.querySelector('.overlayy').classList.add('active');
+});
+
+document.querySelector('.close-btnn').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.remove('active');
+});
+
+
+// 3
+document.querySelector('.buttonn').addEventListener('click', function() {
+    document.querySelector('.overlayy').classList.add('active');
+  });
+  
+  document.querySelector('.close-btnn').addEventListener('click', function() {
+    document.querySelector('.overlayy').classList.remove('active');
+  });
+  
